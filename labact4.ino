332a2a8f23758d9aab72b6213b6b5edcc6876f5b
しるの <92591040+shiruno@users.noreply.github.com>
2024-11-13 19:42:18 +0800

    Add files via upload

---
+++ labact4.ino
@@ -0,0 +1,42 @@
+const int ledPin = 13;           
+const int threshold = 220;       
+bool shouldBlink = false;        
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      
+  Serial.begin(9600);            
+}
+
+void loop() {
+  int rawBrightness = analogRead(A0);     
+  int brightness = map(rawBrightness, 0, 1023, 0, 255);  
+
+
+  Serial.print("Mapped Brightness Level: ");
+  Serial.println(brightness);
+
+  
+  if (brightness >= threshold) {
+    shouldBlink = true;         
+  }
+
+  // Blink the LED if the flag is set
+  if (shouldBlink) {
+    digitalWrite(ledPin, HIGH);  
+    delay(100);                  
+    digitalWrite(ledPin, LOW);   
+    delay(100);                  
+  }
+
+  // Check for serial input to stop blinking
+  if (Serial.available() > 0) {
+    String input = Serial.readString();  
+    input.trim();                        
+
+    
+    if (input.equalsIgnoreCase("stop")) {
+      shouldBlink = false;
+      digitalWrite(ledPin, LOW);         
+    }
+  }
+}
